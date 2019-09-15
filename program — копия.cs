b465f0af6d4ad0ef104fcabed3a03049598ff6c3
akm1982 <akmred@gmail.com>
2019-09-15 11:24:16 +0300

    Сомм 2

---
+++ program — копия.cs
@@ -0,0 +1,14 @@
+using System;
+using System.collections.generic;
+using system.Linq;
+using system.text;
+using system.threading.tasks;
+
+namespace CSharpProj
+{
+    class Program
+    {
+        console.WriteLine("Hello world");
+        console.writeLine("Buy");
+    }
+}
